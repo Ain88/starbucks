a61905eb0db27dfcd5d098f08befb92da00b9295
ain88 <ahyungss@hotmail.com>
2023-04-09 01:20:06 -0700

    Add rest of the project

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener('blur', function() {
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function() {
     console.log(window.scrollY);
@@ -24,13 +25,26 @@ window.addEventListener('scroll', _.throttle(function() {
             opacity: 0,
             display: 'none'
         });
+        gsap.to(toTopEl, .2, {
+            x: 0
+        })
     } else {
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
         });
+        gsap.to(toTopEl, .2, {
+            x: 100
+        })
     }
 }, 300));
+
+
+toTopEl.addEventListener('click', function(){
+    gsap.to(window, .7, {
+        scrollTo: 0
+    });
+})
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
@@ -63,6 +77,16 @@ new Swiper('.promotion .swiper', {
         nextEl: '.promotion .swiper-next'
     }
 });
+new Swiper('.awards .swiper', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
@@ -75,3 +99,36 @@ promotionToggleBtn.addEventListener('click', function() {
         promotionEl.classList.remove('hide');
     }
 });
+
+function random(min, max) {
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+  }
+
+function floatingObject(selector, delay, size) {
+    gsap.to(selector, random(1.5, 2.5), {
+        y: size,
+        repeat: -1,
+        yoyo: true,
+        ease: Power1.easeInOUT,
+        delay: random(0, delay)
+    });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl,
+            triggerHook: .8
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller());
+});
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
